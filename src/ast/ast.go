b3387ce3339cce4c08c2fd862a07fb52c6ae360a
yuyasat <takiyour@gmail.com>
2019-01-05 23:17:33 +0900

    2.4 - Parser's first steps: parsing let statements

---
+++ src/ast/ast.go
@@ -0,0 +1,56 @@
+package ast
+
+import "token"
+
+// Node is [TODO]
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement is [TODO]
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression is [TODO]
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program is [TODO]
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral is [TODO]
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+// LetStatement is [TODO]
+type LetStatement struct {
+	Token token.Token // token.LET トークン
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+// TokenLiteral is [TODO]
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// Identifier is [TODO]
+type Identifier struct {
+	Token token.Token // token.IDENT トークン
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+// TokenLiteral is [TODO]
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
