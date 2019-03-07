f1e941cf577f0828c3f0edaea8f9055f3113a231
yuyasat <takiyour@gmail.com>
2019-03-07 11:47:57 +0900

    3.5.5 - Evaluating Expressions | Prefix Expressions

--- src/evaluator/evaluator_test.go
+++ src/evaluator/evaluator_test.go
@@ -59,6 +59,25 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}
 }
 
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
 func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 	result, ok := obj.(*object.Boolean)
 	if !ok {
