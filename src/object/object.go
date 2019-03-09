3df6140738080aeb6b9fa9d3c58331bf16811c9e
yuyasat <takiyour@gmail.com>
2019-03-09 23:47:22 +0900

    3.7 - Return Statements

--- src/object/object.go
+++ src/object/object.go
@@ -6,9 +6,10 @@ import "fmt"
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 // Object is [TODO]
@@ -47,3 +48,14 @@ func (n *Null) Type() ObjectType { return NULL_OBJ }
 
 // Inspect is [TODO]
 func (n *Null) Inspect() string { return "null" }
+
+// ReturnValue is [TODO]
+type ReturnValue struct {
+	Value Object
+}
+
+// Type is [TODO]
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+
+// Inspect is [TODO]
+func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
