22f14af62ee0d0ec4971395faf2a201d0c738981
yuyasat <takiyour@gmail.com>
2019-03-24 17:27:57 +0900

    3.10 - Functions & Function Calls

--- src/object/environment.go
+++ src/object/environment.go
@@ -1,5 +1,12 @@
 package object
 
+// NewEnclosedEnvironment is [TODO]
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
 // NewEnvironment is [TODO]
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
@@ -9,11 +16,15 @@ func NewEnvironment() *Environment {
 // Environment is [TODO]
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 // Get is [TODO]
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
--- src/object/object.go
+++ src/object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+	"ast"
+	"bytes"
+	"fmt"
+	"strings"
+)
 
 // ObjectType is [TODO]
 type ObjectType string
@@ -11,6 +16,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 // Object is [TODO]
@@ -71,3 +77,32 @@ func (e *Error) Type() ObjectType { return ERROR_OBJ }
 
 // Inspect is [TODO]
 func (e *Error) Inspect() string { return "ERROR: " + e.Message }
+
+// Function is [TODO]
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+// Type is [TODO]
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+
+// Inspect is [TODO]
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
