ea2965db6e584eef2d0991a883f29478bde986a0
yuyasat <takiyour@gmail.com>
2019-03-24 17:41:53 +0900

    4.2.1 - Strings | Supporting Strings in outer Lexer

--- src/lexer/lexer_test.go
+++ src/lexer/lexer_test.go
@@ -24,8 +24,10 @@ func TestNextToken(t *testing.T) {
 		return false;
 	}
 	
-	10 == 10
-	10 != 9
+	10 == 10;
+	10 != 9;
+	"foobar"
+	"foo bar"
 	`
 
 	tests := []struct {
@@ -100,9 +102,13 @@ func TestNextToken(t *testing.T) {
 		{token.INT, "10"},
 		{token.EQ, "=="},
 		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
 		{token.INT, "10"},
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
 		{token.EOF, ""},
 	}
 
--- src/token/token.go
+++ src/token/token.go
@@ -47,6 +47,8 @@ const (
 	IF     = "IF"
 	ELSE   = "ELSE"
 	RETURN = "RETURN"
+
+	STRING = "STRING"
 )
 
 var keywords = map[string]TokenType{
