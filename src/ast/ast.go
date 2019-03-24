1a80531c06b4925c6b9da144213ac194cc6393af
yuyasat <takiyour@gmail.com>
2019-03-24 22:04:32 +0900

    4.2.2 - String | Parsing Strings

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -302,3 +302,15 @@ func (ce *CallExpression) String() string {
 
 	return out.String()
 }
+
+// StringLiteral is [TODO]
+type StringLiteral struct {
+	Token token.Token
+	Value string
+}
+
+func (sl *StringLiteral) expressionNode() {}
+
+// TokenLiteral is [TODO]
+func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
+func (sl *StringLiteral) String() string       { return sl.Token.Literal }
