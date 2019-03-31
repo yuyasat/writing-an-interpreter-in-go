0cb87dfb2ff3000f0d2ff96e6c0e0962891db83a
yuyasat <takiyour@gmail.com>
2019-03-31 17:38:51 +0900

    4.5.2 - Hashes | Parsing Hash Literals

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -362,3 +362,27 @@ func (ie *IndexExpression) String() string {
 
 	return out.String()
 }
+
+// HashLiteral is [TODO]
+type HashLiteral struct {
+	Token token.Token // '{'トークン
+	Pairs map[Expression]Expression
+}
+
+func (hl *HashLiteral) expressionNode() {}
+
+// TokenLiteral is [TODO]
+func (hl *HashLiteral) TokenLiteral() string { return hl.Token.Literal }
+func (hl *HashLiteral) String() string {
+	var out bytes.Buffer
+	pairs := []string{}
+	for key, value := range hl.Pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+
+	return out.String()
+}
